9d2e968be922b5f07cd99a978ef99aa42579a75d
MakotoMachiYuki <joshua1412a.o.t@gmail.com>
2024-11-26 11:56:01 +0700

    Homepage + Angularjs as frontend

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -1,17 +1,21 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="en" ng-app="gatGateApp">
 
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" />
+    <title>@yield('title', 'Default Title')</title>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
     <script src="https://kit.fontawesome.com/8724a9ccea.js" crossorigin="anonymous"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.8.2/angular.min.js"></script>
-    <title>@yield('title', 'Default Title')</title>
+    <script src="js/app.js"></script>
+    <script src="js/productLists.js"></script>
+    <script src="js/controller.js" defer></script>
 </head>
 
 <body>
-    <header>
+    <header ng-controller="HeaderController">
         @yield('header')
     </header>
 
@@ -23,7 +27,7 @@
         @yield('footer')
     </footer>
 </body>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
-@yield('scripts')
+
+
 
 </html>
--- routes/web.php
+++ routes/web.php
@@ -4,10 +4,13 @@ use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\Accounts\CreateAccountController;
 use App\Http\Controllers\Accounts\LoginAccountController;
+use App\Http\Controllers\HomePageController;
 
 
-Route::get('/', [LoginAccountController::class, 'loginAccountView'])->name('loginAccountView');
-Route::post('/', [LoginAccountController::class, 'loginAccount'])->name('loginAccount');
+
+Route::get('/', [HomePageController::class, 'homePageView'])->name('homePageView');
+Route::get('/login', [LoginAccountController::class, 'loginAccountView'])->name('loginAccountView');
+Route::post('/login', [LoginAccountController::class, 'loginAccount'])->name('loginAccount');
 Route::get('/signup', [CreateAccountController::class, 'createAccountView'])->name('createAccountView');
 Route::get('/signup', [CreateAccountController::class, 'createAccountView'])->name('createAccountView');
 Route::post('/signup', [CreateAccountController::class, 'createAccount'])->name('createAccount');
