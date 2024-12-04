47e3eb9a1b3b2350b281c482217d16ba81bd9f43
Viviann09 <viviantan0910@gmail.com>
2024-12-04 15:48:12 +0700

    Comment CRUD

--- public/js/controllers/ProductDetailsController.js
+++ public/js/controllers/ProductDetailsController.js
@@ -13,5 +13,38 @@ app.controller("ProductDetailsController", [
                 console.error("Error fetching product details:", error);
                 $scope.error = "Unable to fetch product details.";
             });
+
+    $scope.comments = [];
+        $http
+            .get(`/api/product/${productName}/comments`)
+            .then(function (response) {
+                $scope.comments = response.data;
+            })
+            .catch(function (error) {
+                console.error("Error fetching comments:", error);
+                $scope.error = "Unable to fetch product comments.";
+            });
+
+        $scope.newComment = {
+            user: "",
+            content: ""
+        };
+
+        $scope.addComment = function () {
+            if ($scope.newComment.content) {
+                $http
+                    .post(`/api/product/${productName}/comments`, $scope.newComment)
+                    .then(function (response) {
+                        $scope.comments.push(response.data);
+                        $scope.newComment.content = ""; 
+                    })
+                    .catch(function (error) {
+                        console.error("Error adding comment:", error);
+                    });
+            }
+        };
+
+
     },
+    
 ]);
